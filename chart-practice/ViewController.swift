6957d087c4c11b8406c6ab8b1388f07dcdaa7850
HeonJin Ha <weizen511@gmail.com>
2023-10-04 00:56:19 +0900

    feat: Bar Chart 기본 구성 작업

--- chart-practice/ViewController.swift
+++ chart-practice/ViewController.swift
@@ -6,25 +6,103 @@
 //
 
 import UIKit
-import Charts
+import DGCharts
 
 class ViewController: UIViewController {
+    
+    // MARK: Main View
+    private lazy var barChartView: BarChartView = createChart()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        chartAxis()
+        configureLegend()
+        setData()
     }
     
-    private func createChart() {
-        // create bar chart
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        layout()
+    }
+    
+    private func layout() {
+        view.addSubview(barChartView)
+    }
+    
+    // MARK: - Chart View 생성
+    private func createChart() -> BarChartView {
         
-        // configure axis
+        let width = view.frame.size.width
+        let frame = CGRect(x: 0, y: 0, width: width, height: width)
+        let barChartView = BarChartView(frame: frame)
+        barChartView.center = view.center
+        barChartView.fitBars = true
+        barChartView.delegate = self
+        return barChartView
+    }
+    
+    // MARK: - Axis Layout : 범례 객체
+    private func chartAxis() {
+        // MARK: AXIS (선 / font, line, color 등등 구성 변경)
+        let xAxis = barChartView.xAxis
+        xAxis.gridLineWidth = 0
         
-        // configure legend
+        let rightAxis = barChartView.rightAxis
+        rightAxis.gridColor = .blue
+        rightAxis.gridLineWidth = 0
+
+        let leftAxis = barChartView.leftAxis
+        leftAxis.gridColor = .red
+        leftAxis.gridLineWidth = 0
+    }
+    
+    // MARK: - `Legend`: 범례 객체
+    private func configureLegend() {
+        let legend = barChartView.legend
+        legend.form = .circle
+        legend.direction = .rightToLeft
+      
+    }
+    
+    // MARK: - DATA ENTRY (데이터)
+    private func setData() {
+        var entries = [BarChartDataEntry]()
+        for x in 0..<10 {
+            let entry = BarChartDataEntry(x: Double(x),
+                                          y: Double.random(in: 0...30))
+            entries.append(entry)
+        }
         
-        // supply data
+        // MARK: SET
+        let set = BarChartDataSet(entries: entries, label: "Cost")
+        set.colors = ChartColorTemplates.material()
         
+        // MARK: DATA
+        let data = BarChartData(dataSet: set)
+        data.barWidth = 0.8
+        data.isHighlightEnabled = true
+        
+        // 데이터 적용
+        barChartView.data = data
     }
 
-
 }
+
+// MARK: - Chart View DELEGATE
+extension ViewController: ChartViewDelegate {
+    
+}
+
+#if DEBUG
+import SwiftUI
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview {
+            ViewController()
+        }
+        .ignoresSafeArea()
+    }
+}
+#endif
 
